e4c09a221826aaff9a3416c2a4ad26a8d34f4520
Mapper <killermapper@outlook.com>
2020-02-12 13:12:37 +0100

    Imported registry methods from EmbellishCraft + updated code.

--- src/main/java/tv/mapper/mapperbase/block/BaseBlocks.java
+++ src/main/java/tv/mapper/mapperbase/block/BaseBlocks.java
@@ -1,11 +1,9 @@
 package tv.mapper.mapperbase.block;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.PressurePlateBlock.Sensitivity;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent;
@@ -15,6 +13,7 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ObjectHolder;
 import tv.mapper.mapperbase.MapperBase;
 import tv.mapper.mapperbase.item.BaseGroups;
+import tv.mapper.mapperbase.util.RegistryUtils;
 
 @ObjectHolder(MapperBase.MODID)
 @EventBusSubscriber(bus = Bus.MOD)
@@ -26,32 +25,29 @@ public class BaseBlocks
     public static final Block STEEL_WALL = null;
     public static final Block STEEL_PRESSURE_PLATE = null;
 
+    public static final Block CONCRETE = null;
+    public static final Block CONCRETE_STAIRS = null;
+    public static final Block CONCRETE_SLAB = null;
+    public static final Block CONCRETE_WALL = null;
+    public static final Block CONCRETE_PRESSURE_PLATE = null;
+
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
-        Block block;
         event.getRegistry().register(
-            block = new Block(Block.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(5.0F, 6.0F).sound(SoundType.LANTERN)).setRegistryName("steel_block"));
+            new CustomBlock(Block.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(5.0F, 6.0F).sound(SoundType.LANTERN), ToolType.PICKAXE).setRegistryName("steel_block"));
+        RegistryUtils.createOnlyVariants(event, "steel", true, true, true, true, Material.IRON, MaterialColor.STONE, SoundType.LANTERN, ToolType.PICKAXE, 5.0F, 6.0F);
+
         event.getRegistry().register(
-            new CustomSlabBlock(Block.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(5.0F, 6.0F).sound(SoundType.LANTERN), ToolType.PICKAXE).setRegistryName(
-                "steel_slab"));
-        event.getRegistry().register(new CustomStairsBlock(() -> block.getDefaultState(), Block.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(5.0F, 6.0F).sound(
-            SoundType.LANTERN), ToolType.PICKAXE).setRegistryName("steel_stairs"));
-        event.getRegistry().register(
-            new CustomWallBlock(Block.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(5.0F, 6.0F).sound(SoundType.LANTERN), ToolType.PICKAXE).setRegistryName(
-                "steel_wall"));
-        event.getRegistry().register(
-            new CustomPressurePlateBlock(Sensitivity.MOBS, Block.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(5.0F, 6.0F).sound(SoundType.LANTERN)).setRegistryName(
-                "steel_pressure_plate"));
+            new CustomBlock(Block.Properties.create(Material.ROCK, MaterialColor.STONE).hardnessAndResistance(1.5F, 6.0F).sound(SoundType.STONE), ToolType.PICKAXE).setRegistryName("concrete"));
+        RegistryUtils.createOnlyVariants(event, "concrete", true, true, true, true, Material.ROCK, MaterialColor.STONE, SoundType.STONE, ToolType.PICKAXE, 1.5F, 6.0F);
+
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
-        event.getRegistry().register(new BlockItem(STEEL_BLOCK, new Item.Properties().group(BaseGroups.MAPPERBASE)).setRegistryName(STEEL_BLOCK.getRegistryName()));
-        event.getRegistry().register(new BlockItem(STEEL_SLAB, new Item.Properties().group(BaseGroups.MAPPERBASE)).setRegistryName(STEEL_SLAB.getRegistryName()));
-        event.getRegistry().register(new BlockItem(STEEL_STAIRS, new Item.Properties().group(BaseGroups.MAPPERBASE)).setRegistryName(STEEL_STAIRS.getRegistryName()));
-        event.getRegistry().register(new BlockItem(STEEL_WALL, new Item.Properties().group(BaseGroups.MAPPERBASE)).setRegistryName(STEEL_WALL.getRegistryName()));
-        event.getRegistry().register(new BlockItem(STEEL_PRESSURE_PLATE, new Item.Properties().group(BaseGroups.MAPPERBASE)).setRegistryName(STEEL_PRESSURE_PLATE.getRegistryName()));
+        RegistryUtils.createItemBlockWithVariants(event, BaseGroups.MAPPERBASE, STEEL_BLOCK, STEEL_STAIRS, STEEL_SLAB, STEEL_WALL, STEEL_PRESSURE_PLATE);
+        RegistryUtils.createItemBlockWithVariants(event, BaseGroups.MAPPERBASE, CONCRETE, CONCRETE_STAIRS, CONCRETE_SLAB, CONCRETE_WALL, CONCRETE_PRESSURE_PLATE);
     }
 }
