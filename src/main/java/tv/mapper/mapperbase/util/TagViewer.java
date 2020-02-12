86e2db9782b2a8d8595ca597da169117060f8829
Mapper <killermapper@outlook.com>
2020-02-12 16:37:59 +0100

    Added tag viewer. Will add config later to enable / disable it.

---
+++ src/main/java/tv/mapper/mapperbase/util/TagViewer.java
@@ -0,0 +1,45 @@
+package tv.mapper.mapperbase.util;
+
+import java.util.List;
+import java.util.Map;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.item.Item;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class TagViewer
+{
+    @SubscribeEvent
+    public static void onTooltip(ItemTooltipEvent e)
+    {
+        if(!Screen.hasControlDown())
+            return;
+        List<ITextComponent> tooltips = e.getToolTip();
+        Item item = e.getItemStack().getItem();
+        Map<ResourceLocation, Tag<Item>> tagmap = ItemTags.getCollection().getTagMap();
+
+        int count = 0;
+
+        for(Map.Entry<ResourceLocation, Tag<Item>> entry : tagmap.entrySet())
+        {
+            if(item.isIn(entry.getValue()))
+            {
+                tooltips.add(new StringTextComponent("Tag: " + entry.getKey().toString()).applyTextStyle(TextFormatting.ITALIC).applyTextStyle(TextFormatting.DARK_GRAY));
+                count++;
+            }
+        }
+
+        if(count <= 0)
+            tooltips.add(new StringTextComponent("No tag found...").applyTextStyle(TextFormatting.ITALIC).applyTextStyle(TextFormatting.DARK_GRAY));
+    }
+}
