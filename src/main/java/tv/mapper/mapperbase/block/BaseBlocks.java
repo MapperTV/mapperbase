732b8906656da0985cbd04775a2b16a83b888216
Mapper <killermapper@outlook.com>
2020-02-18 17:07:11 +0100

    Added concrete fence.

--- src/main/java/tv/mapper/mapperbase/block/BaseBlocks.java
+++ src/main/java/tv/mapper/mapperbase/block/BaseBlocks.java
@@ -33,6 +33,7 @@ public class BaseBlocks
     public static final Block CONCRETE_SLAB = null;
     public static final Block CONCRETE_WALL = null;
     public static final Block CONCRETE_PRESSURE_PLATE = null;
+    public static final Block CONCRETE_FENCE = null;
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
@@ -46,6 +47,8 @@ public class BaseBlocks
         event.getRegistry().register(
             new CustomBlock(Block.Properties.create(Material.ROCK, MaterialColor.STONE).hardnessAndResistance(1.5F, 6.0F).sound(SoundType.STONE), ToolType.PICKAXE).setRegistryName("concrete"));
         RegistryUtils.createOnlyVariants(event, "concrete", true, true, true, true, Material.ROCK, MaterialColor.STONE, SoundType.STONE, ToolType.PICKAXE, 1.5F, 6.0F);
+        event.getRegistry().register(
+            new FenceBlock(Block.Properties.create(Material.ROCK, MaterialColor.STONE).hardnessAndResistance(1.5F, 6.0F).sound(SoundType.STONE)).setRegistryName("concrete_fence"));
     }
 
     @SubscribeEvent
@@ -54,5 +57,6 @@ public class BaseBlocks
         RegistryUtils.createItemBlockWithVariants(event, BaseGroups.MAPPERBASE, STEEL_BLOCK, STEEL_STAIRS, STEEL_SLAB, STEEL_WALL, STEEL_PRESSURE_PLATE);
         event.getRegistry().register(new BlockItem(STEEL_FENCE, new Item.Properties().group(BaseGroups.MAPPERBASE)).setRegistryName(STEEL_FENCE.getRegistryName()));
         RegistryUtils.createItemBlockWithVariants(event, BaseGroups.MAPPERBASE, CONCRETE, CONCRETE_STAIRS, CONCRETE_SLAB, CONCRETE_WALL, CONCRETE_PRESSURE_PLATE);
+        event.getRegistry().register(new BlockItem(CONCRETE_FENCE, new Item.Properties().group(BaseGroups.MAPPERBASE)).setRegistryName(CONCRETE_FENCE.getRegistryName()));
     }
 }
