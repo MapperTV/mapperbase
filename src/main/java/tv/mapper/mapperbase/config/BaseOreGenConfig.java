d324a28905753113a5f1d0f9a3f04aacc97f9078
Mapper <killermapper@outlook.com>
2020-04-20 18:19:49 +0200

    Added biome black/whitelist for bitumen ore generation.

--- src/main/java/tv/mapper/mapperbase/config/BaseOreGenConfig.java
+++ src/main/java/tv/mapper/mapperbase/config/BaseOreGenConfig.java
@@ -1,8 +1,11 @@
 package tv.mapper.mapperbase.config;
 
+import java.util.ArrayList;
+
 import org.apache.commons.lang3.tuple.Pair;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class BaseOreGenConfig
 {
@@ -24,9 +27,16 @@ public class BaseOreGenConfig
         public static ForgeConfigSpec.IntValue BITUMEN_SIZE;
         public static ForgeConfigSpec.IntValue BITUMEN_MIN_HEIGHT;
         public static ForgeConfigSpec.IntValue BITUMEN_MAX_HEIGHT;
+        public static ForgeConfigSpec.BooleanValue BITUMEN_LIST_MODE;
+        public static ConfigValue<ArrayList<String>> BITUMEN_BIOME_LIST;
+
+        private static ArrayList<String> default_biomes = new ArrayList<>();
 
         CommonConfig(ForgeConfigSpec.Builder COMMON_BUILDER)
         {
+            default_biomes.add("minecraft:nether");
+            default_biomes.add("minecraft:the_end");
+
             COMMON_BUILDER.comment("Bitumen Generation").push("bitumen");
 
             BITUMEN_GENERATION = COMMON_BUILDER.comment("Generate bitumen in the world [true / false]").define("bitumenGeneration", true);
@@ -34,6 +44,14 @@ public class BaseOreGenConfig
             BITUMEN_CHANCE = COMMON_BUILDER.comment("Chances of bitumen pocket being generated [0-100, default: 25]").defineInRange("bitumenChance", 25, 0, 100);
             BITUMEN_MIN_HEIGHT = COMMON_BUILDER.comment("Minimal height for bitumen pocket generation, [0-255, default: 40]").defineInRange("bitumenMinHeight", 40, 0, 255);
             BITUMEN_MAX_HEIGHT = COMMON_BUILDER.comment("Maximal height for bitumen pocket generation [0-255, default: 160]").defineInRange("bitumenMaxHeight", 160, 0, 255);
+
+            COMMON_BUILDER.pop();
+            COMMON_BUILDER.comment(
+                "Biome Blacklist: list here the biomes you do not want bitumen ore to be generated in (syntax: \"modid:biome\". Examples: \"minecraft:plains\", \"biomesoplenty:meadow\"...). Note: you can replace the default values, bitumen ore only generates where stone is. It never generates in the Nether or the End.").push(
+                    "blacklist");
+
+            BITUMEN_LIST_MODE = COMMON_BUILDER.comment("Set to true to switch bitumen ore blacklist to whitelist mode [true / false]").define("bitumenWhitelist", false);
+            BITUMEN_BIOME_LIST = COMMON_BUILDER.comment("List of blacklisted biomes for bitumen ore generation").define("bitumenBiomeBlacklist", default_biomes);
 
             COMMON_BUILDER.pop();
         }
--- src/main/java/tv/mapper/mapperbase/world/BaseOreGenerator.java
+++ src/main/java/tv/mapper/mapperbase/world/BaseOreGenerator.java
@@ -1,5 +1,7 @@
 package tv.mapper.mapperbase.world;
 
+import java.util.ArrayList;
+
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
@@ -7,21 +9,60 @@ import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.registries.ForgeRegistries;
+import tv.mapper.mapperbase.MapperBase;
 import tv.mapper.mapperbase.block.BaseBlocks;
 import tv.mapper.mapperbase.config.BaseOreGenConfig.CommonConfig;
 
 public class BaseOreGenerator
 {
+    private static boolean generate;
+
     public static void setupOregen()
     {
+        // Check if config is correct
+        ArrayList<String> biome_verify = new ArrayList<>();
+        for(Biome biome : ForgeRegistries.BIOMES)
+        {
+            biome_verify.add(biome.getRegistryName().toString());
+        }
+
+        for(String name : CommonConfig.BITUMEN_BIOME_LIST.get())
+        {
+            if(!biome_verify.contains(name))
+                MapperBase.LOGGER.warn("Invalid biome '" + name + "' in mapperbase-common.toml in bitumen ore blacklist, please verify the entries!");
+        }
+
         for(Biome biome : ForgeRegistries.BIOMES)
         {
             if(CommonConfig.BITUMEN_GENERATION.get())
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
-                    Feature.ORE.withConfiguration(
+            {
+                if(CommonConfig.BITUMEN_BIOME_LIST.get().contains(biome.getRegistryName().toString()))
+                {
+                    if(CommonConfig.BITUMEN_LIST_MODE.get())
+                    {
+                        MapperBase.LOGGER.info("Bitumen ore whitelisted for biome " + biome.getRegistryName() + " in the config.");
+                        generate = true;
+                    }
+                    else
+                    {
+                        MapperBase.LOGGER.info("Bitumen ore blacklisted for biome " + biome.getRegistryName() + " in the config.");
+                        generate = false;
+                    }
+                }
+                else
+                {
+                    if(CommonConfig.BITUMEN_LIST_MODE.get())
+                        generate = false;
+                    else
+                        generate = true;
+                }
+
+                if(generate)
+                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
                         new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BaseBlocks.BITUMEN_ORE.get().getDefaultState(), CommonConfig.BITUMEN_SIZE.get().intValue())).withPlacement(
                             Placement.COUNT_RANGE.configure(
                                 new CountRangeConfig(CommonConfig.BITUMEN_CHANCE.get(), CommonConfig.BITUMEN_MIN_HEIGHT.get(), CommonConfig.BITUMEN_MIN_HEIGHT.get(), CommonConfig.BITUMEN_MAX_HEIGHT.get()))));
+            }
         }
     }
 }
