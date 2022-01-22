25cd8404073d0747274dba5ae1c5171109d5f166
Mapper <official.mapper@gmail.com>
2022-01-22 13:37:44 -0500

    Forge update + ore gen

--- src/main/java/tv/mapper/mapperbase/world/BaseOreGenerator.java
+++ src/main/java/tv/mapper/mapperbase/world/BaseOreGenerator.java
@@ -1,6 +1,13 @@
 package tv.mapper.mapperbase.world;
 
+import java.util.List;
+
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
+import net.minecraft.world.level.levelgen.placement.CountPlacement;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.minecraft.world.level.levelgen.placement.RarityFilter;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -49,5 +56,20 @@ public class BaseOreGenerator
                 }
             }
         }
+    }
+
+    public static List<PlacementModifier> orePlacement(PlacementModifier amount, PlacementModifier modifier)
+    {
+        return List.of(amount, InSquarePlacement.spread(), modifier, BiomeFilter.biome());
+    }
+
+    public static List<PlacementModifier> commonOrePlacement(int amount, PlacementModifier modifier)
+    {
+        return orePlacement(CountPlacement.of(amount), modifier);
+    }
+
+    public static List<PlacementModifier> rareOrePlacement(int amount, PlacementModifier modifier)
+    {
+        return orePlacement(RarityFilter.onAverageOnceEvery(amount), modifier);
     }
 }
